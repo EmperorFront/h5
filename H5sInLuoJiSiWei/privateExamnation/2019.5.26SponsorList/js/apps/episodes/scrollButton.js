2f3b2343f00c025e5fb3f476f81096284b893f35
liuxicheng <liuxicheng@luojilab.com>
2019-05-15 16:53:13 +0800

    提交按钮

--- H5sInLuoJiSiWei/privateExamnation/2019.5.26SponsorList/js/apps/episodes/scrollButton.js
+++ H5sInLuoJiSiWei/privateExamnation/2019.5.26SponsorList/js/apps/episodes/scrollButton.js
@@ -1,7 +1,9 @@
 var onetime_click = 1;
 var Asio = Asio || {send:function(){}};
 // 初始化
-document.querySelector('#scroll-button').innerHTML = "<img src='https://piccdn.luojilab.com/fe-oss/default/MTU1NzkwODY5MzYz.png' style='width:100%;' onclick='jump();'>";
+$(function(){
+	document.querySelector('#scroll-button').innerHTML = "<img src='https://piccdn.luojilab.com/fe-oss/default/MTU1NzkwODY5MzYz.png' style='width:100%;' onclick='jump();'>";
+});
 function jump() {
 	if(onetime_click){
 		onetime_click = 0;	
