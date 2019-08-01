3258d1184f7809d9932d0655a2844a61e61d867d
liuxicheng <liuxicheng@luojilab.com>
2019-08-01 18:16:48 +0800

    gd

--- H5sInLuoJiSiWei/privateExamnation/2019.6.11ListenBook/js/index0801.js
+++ H5sInLuoJiSiWei/privateExamnation/2019.6.11ListenBook/js/index0801.js
@@ -632,6 +632,24 @@ function setDarg() {//拖动逻辑
     } else if(currentScroll === totalScroll) {
       document.scrollTop = top - 1
     }
+
+
+
+    //禁止向上拉
+    var el = window.document.getElementById("event")
+    var topss = el.scrollTop
+    , totalScroll = el.scrollHeight
+    , currentScroll = topss + el.offsetHeight;
+    //If we're at the top or the bottom of the containers
+    //scroll, push up or down one pixel.
+    //
+    //this prevents the scroll from "passing through" to
+    //the body.
+    if(topss === 0) {
+      el.scrollTop = 1;
+    } else if(currentScroll === totalScroll) {
+      el.scrollTop = topss - 1;
+    }
   })
 
   window.document.getElementById("event").addEventListener('touchmove', function(e) {
@@ -1859,48 +1877,10 @@ setInterval(() => {
 
  
 
-    //禁止下拉
-    var overscroll = function(el) {
-      el.addEventListener('touchstart', function() {
-        var top = el.scrollTop
-          , totalScroll = el.scrollHeight
-          , currentScroll = top + el.offsetHeight
-        //If we're at the top or the bottom of the containers
-        //scroll, push up or down one pixel.
-        //
-        //this prevents the scroll from "passing through" to
-        //the body.
-        if(top === 0) {
-          el.scrollTop = 1
-        } else if(currentScroll === totalScroll) {
-          el.scrollTop = top - 1
-        }
-      })
-      el.addEventListener('touchmove', function(evt) {
-        //if the content is actually scrollable, i.e. the content is long enough
-        //that scrolling can occur
-        if(el.offsetHeight < el.scrollHeight)
-          evt._isScroller = true
-      })
-    }
-    overscroll(document.getElementById('root'));
-    document.body.addEventListener('touchmove', function(evt) {
-      //In this case, the default behavior is scrolling the body, which
-      //would result in an overflow.  Since we don't want that, we preventDefault.
-      if(!evt._isScroller) {
-        evt.preventDefault = function(){
-          return false;
-        }
-        evt.preventDefault();
 
 
 
-
-
-      }
-    })
-
-var touch2 = function () {
+var touch2 = function () {debugger;
   document.addEventListener('touchmove', function(e){e.preventDefault()}, false);
   }
 
