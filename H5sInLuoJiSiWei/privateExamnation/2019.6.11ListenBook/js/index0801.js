f9994b46f1c043014e01706e120e583f1df0c628
liuxicheng <liuxicheng@luojilab.com>
2019-08-02 18:16:52 +0800

    tp

--- H5sInLuoJiSiWei/privateExamnation/2019.6.11ListenBook/js/index0801.js
+++ H5sInLuoJiSiWei/privateExamnation/2019.6.11ListenBook/js/index0801.js
@@ -608,67 +608,6 @@ function setDarg() {
 
 
 
-  // 首先禁止body
-document.body.ontouchmove = function (e) {
-  e.preventDefault();
-};
-
-// 然后取得触摸点的坐标
-var startX = 0, startY = 0;
-//touchstart事件
-function touchSatrtFunc(evt) {
-try
-{
-    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
-    var touch = evt.touches[0]; //获取第一个触点
-    var x = Number(touch.pageX); //页面触点X坐标
-    var y = Number(touch.pageY); //页面触点Y坐标
-    //记录触点初始位置
-    startX = x;
-    startY = y;
-
-} catch (e) {
-    // alert('touchSatrtFunc：' + e.message);
-}
-}
-document.addEventListener('touchstart', touchSatrtFunc, false);
-
-// 然后对允许滚动的条件进行判断，这里讲滚动的元素指向body
-var _ss = document.body;
-_ss.ontouchmove = function (ev) {
-var _point = ev.touches[0],
-        _top = _ss.scrollTop;
-// 什么时候到底部
-var _bottomFaVal = _ss.scrollHeight - _ss.offsetHeight;
-// 到达顶端
-if (_top === 0) {
-    // 阻止向下滑动
-    if (_point.clientY > startY) {
-        ev.preventDefault();
-    } else {
-        // 阻止冒泡
-        // 正常执行
-        ev.stopPropagation();
-    }
-} else if (_top === _bottomFaVal) {
-    // 到达底部 如果想禁止页面滚动和上拉加载，讲这段注释放开，也就是在滚动到页面底部的制售阻止默认事件
-    // 阻止向上滑动
-    // if (_point.clientY < startY) {
-    //     ev.preventDefault();
-    // } else {
-    //     // 阻止冒泡
-    //     // 正常执行
-    //     ev.stopPropagation();
-    // }
-} else if (_top > 0 && _top < _bottomFaVal) {
-    ev.stopPropagation();
-} else {
-    ev.preventDefault();
-}
-};
-
-
-
   
   
   
@@ -725,59 +664,123 @@ if (_top === 0) {
     }
   })
 
-  window.document.getElementById("root").addEventListener('touchmove', function(e) {
-    if(!canTouch){
-      return;
-    }
-    var nowDeg = {}
-    var nowDeg2 = {} // 悬浮层
-    var nowPoint = {}
-    nowPoint.x = e.changedTouches[0].pageX;
-    nowPoint.y = e.changedTouches[0].pageY;
-    var dis = {}
-    dis.x = nowPoint.x - startPoint.x
-    dis.y = nowPoint.y - startPoint.y
-    var disDeg = {}
-    disDeg.x = -(dis.x / scale.x) // 距离转度数
-    disDeg.y = dis.y / scale.y
-    // panoBgDeg.y = css(panoBg, 'rotateX')
 
-    nowDeg.y = panoBgDeg.y + disDeg.y // 开始角度 + 移动角度
-    nowDeg.x = panoBgDeg.x + disDeg.x
-    nowDeg2.x = panoBgDeg.x + (disDeg.x) * 0.95
-    nowDeg2.y = panoBgDeg.y + (disDeg.y) * 0.95
-    if (nowDeg.y > 45) {
-      nowDeg.y = 45
-    } else if (nowDeg.y < -45) {
-      nowDeg.y = -45
-    }
 
-    if (nowDeg2.y > 45) {
-      nowDeg2.y = 45
-    } else if (nowDeg2.y < -45) {
-      nowDeg2.y = -45
-    }
-    lastDis.x = nowDeg.x - lastDeg.x
-    lastDeg.x = nowDeg.x
-    lastDis.y = nowDeg.y - lastDeg.y
-    lastDeg.y = nowDeg.y
-    css(panoBg, "rotateX", nowDeg.y);
-    css(panoBg, "rotateY", nowDeg.x);
-    css(pano, "rotateX", nowDeg2.y); //pano为浮动层，rotateX为上下，改动的是y
-    css(pano, "rotateY", nowDeg2.x);//nowDeg2.x这个值是错误的。
-    var disZ = Math.max(Math.abs(dis.x), Math.abs(dis.y))
-    if (disZ > 300) {
-      disZ = 300
-    }
-    css(tZ, 'translateZ', startZ - disZ);
 
-    doAnimationAboutPostion(nowDeg.x);
 
-    e.stopPropagation();
-    e.cancelBubble = true;
-    return false;
+
+
+ // 首先禁止body
+ document.body.ontouchmove = function (e) {
+  e.preventDefault();
+};
+
+// 然后取得触摸点的坐标
+var startX = 0, startY = 0;
+
+// 然后对允许滚动的条件进行判断，这里讲滚动的元素指向body
+var _ss = document.body;
+_ss.ontouchmove = function (ev) {
+var _point = ev.touches[0],
+    _top = _ss.scrollTop;
+// 什么时候到底部
+var _bottomFaVal = _ss.scrollHeight - _ss.offsetHeight;
+// 到达顶端
+if (_top === 0) {
+    // 阻止向下滑动
+    if (_point.clientY > startY) {
+        ev.preventDefault();
+    } else {
+        // 阻止冒泡
+        // 正常执行
+        ev.stopPropagation();
+    }
+} else if (_top === _bottomFaVal) {
+    // 到达底部 如果想禁止页面滚动和上拉加载，讲这段注释放开，也就是在滚动到页面底部的制售阻止默认事件
+    // 阻止向上滑动
+    // if (_point.clientY < startY) {
+    //     ev.preventDefault();
+    // } else {
+    //     // 阻止冒泡
+    //     // 正常执行
+    //     ev.stopPropagation();
+    // }
+} else if (_top > 0 && _top < _bottomFaVal) {
+
+
+
+  //正常逻辑
+if(!canTouch){
+  return;
+}
+var nowDeg = {}
+var nowDeg2 = {} // 悬浮层
+var nowPoint = {}
+nowPoint.x = ev.changedTouches[0].pageX;
+nowPoint.y = ev.changedTouches[0].pageY;
+var dis = {}
+dis.x = nowPoint.x - startPoint.x
+dis.y = nowPoint.y - startPoint.y
+var disDeg = {}
+disDeg.x = -(dis.x / scale.x) // 距离转度数
+disDeg.y = dis.y / scale.y
+// panoBgDeg.y = css(panoBg, 'rotateX')
+
+nowDeg.y = panoBgDeg.y + disDeg.y // 开始角度 + 移动角度
+nowDeg.x = panoBgDeg.x + disDeg.x
+nowDeg2.x = panoBgDeg.x + (disDeg.x) * 0.95
+nowDeg2.y = panoBgDeg.y + (disDeg.y) * 0.95
+if (nowDeg.y > 45) {
+  nowDeg.y = 45
+} else if (nowDeg.y < -45) {
+  nowDeg.y = -45
+}
+
+if (nowDeg2.y > 45) {
+  nowDeg2.y = 45
+} else if (nowDeg2.y < -45) {
+  nowDeg2.y = -45
+}
+lastDis.x = nowDeg.x - lastDeg.x
+lastDeg.x = nowDeg.x
+lastDis.y = nowDeg.y - lastDeg.y
+lastDeg.y = nowDeg.y
+css(panoBg, "rotateX", nowDeg.y);
+css(panoBg, "rotateY", nowDeg.x);
+css(pano, "rotateX", nowDeg2.y); //pano为浮动层，rotateX为上下，改动的是y
+css(pano, "rotateY", nowDeg2.x);//nowDeg2.x这个值是错误的。
+var disZ = Math.max(Math.abs(dis.x), Math.abs(dis.y))
+if (disZ > 300) {
+  disZ = 300
+}
+css(tZ, 'translateZ', startZ - disZ);
+
+doAnimationAboutPostion(nowDeg.x);
+
+ev.stopPropagation();
+ev.cancelBubble = true;
+return false;
+
+
+
+} else {
+    ev.preventDefault();
+}
+};
+
+
+
+
+
+
+
+
+
+
+  // window.document.getElementById("root").addEventListener('touchmove', function(e) {
     
-  })
+    
+  // })
   window.document.getElementById("root").addEventListener('touchend', function(e) {
 
     var nowDeg = {
